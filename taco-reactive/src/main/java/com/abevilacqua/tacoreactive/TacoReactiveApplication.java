5437f561a6b3ebe45bca706ba5e397d5b7bc88fe
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-02 14:53:42 +0200

    Adds Taco-Reactive: a Spring Reactor implementation of Taco store

---
+++ taco-reactive/src/main/java/com/abevilacqua/tacoreactive/TacoReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.abevilacqua.tacoreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoReactiveApplication.class, args);
+	}
+
+}
---
+++ taco-reactive/src/test/java/com/abevilacqua/tacoreactive/TacoReactiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abevilacqua.tacoreactive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TacoReactiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
