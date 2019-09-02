1be6fcfb031610715dffaca1cf9484536d401f64
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-02 16:25:33 +0200

    Adds version of embed mongodb to properties. Removes annotation from Taco. Adds mongodb properties.

--- taco-reactive/src/main/java/com/abevilacqua/tacoreactive/TacoReactiveApplication.java
+++ taco-reactive/src/main/java/com/abevilacqua/tacoreactive/TacoReactiveApplication.java
@@ -2,14 +2,18 @@ package com.abevilacqua.tacoreactive;
 
 import com.abevilacqua.tacoreactive.repo.IngredientRepository;
 import com.abevilacqua.tacoreactive.repo.TacoRepository;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 import static com.abevilacqua.tacoreactive.config.DBInitializer.initialize;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class TacoReactiveApplication {
 
 	public static void main(String[] args) {
--- taco-reactive/src/main/java/com/abevilacqua/tacoreactive/model/Taco.java
+++ taco-reactive/src/main/java/com/abevilacqua/tacoreactive/model/Taco.java
@@ -8,8 +8,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import reactor.core.publisher.Flux;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -22,13 +20,10 @@ public class Taco {
     @Id
     private Long id;
 
-    @NotNull
-    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
     private Date createdAt;
 
-    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private Flux<Ingredient> ingredients;
 
     void createdAt() { this.createdAt = new Date(); }
