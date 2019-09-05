05577c4f9bd980e7b55de94bcbfed67e806d8fd5
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-05 15:40:09 +0200

    Adds new method in TacoRepo

---
+++ taco-reactive/src/main/java/com/abevilacqua/tacoreactive/model/Order.java
@@ -0,0 +1,30 @@
+package com.abevilacqua.tacoreactive.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Document
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+public class Order implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  private String id;
+
+  private LocalDate placedAt;
+
+  private List<Taco> tacos;
+
+  public void addTaco(Taco taco) {
+    this.tacos.add(taco);
+  }
+}
--- taco-reactive/src/main/java/com/abevilacqua/tacoreactive/repo/TacoRepository.java
+++ taco-reactive/src/main/java/com/abevilacqua/tacoreactive/repo/TacoRepository.java
@@ -3,8 +3,10 @@ package com.abevilacqua.tacoreactive.repo;
 import com.abevilacqua.tacoreactive.model.Taco;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface TacoRepository extends ReactiveCrudRepository<Taco, Long> {
 
+  Flux<Taco> findByOrOrderByCreatedAtDesc();
 }
