679b4fdf6d77de784019d16302ff7b13da9595b6
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-05 10:27:31 +0200

    Adds security configuration reactive

--- taco-reactive/src/main/java/com/abevilacqua/tacoreactive/config/SecurityConfig.java
+++ taco-reactive/src/main/java/com/abevilacqua/tacoreactive/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.abevilacqua.tacoreactive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
