54fa637f75969286e0637bb8fc9f364b66bccac2
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-06 14:07:07 +0200

    Adds taco-service-registry project, to use Spring Eureka Server

---
+++ taco-service-registry/src/main/java/com/abevilacqua/tacoserviceregistry/TacoServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.abevilacqua.tacoserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoServiceRegistryApplication.class, args);
+	}
+
+}
