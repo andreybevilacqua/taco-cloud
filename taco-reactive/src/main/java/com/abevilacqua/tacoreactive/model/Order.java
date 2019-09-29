73163f793a5c7bc55fc509cb8a7d3e96ad44ab16
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-29 17:37:44 +0200

    Removes unused methods

--- taco-reactive/src/main/java/com/abevilacqua/tacoreactive/model/Order.java
+++ taco-reactive/src/main/java/com/abevilacqua/tacoreactive/model/Order.java
@@ -12,7 +12,6 @@ import java.util.List;
 @Document
 @RequiredArgsConstructor
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class Order implements Serializable {
 
   private static final long serialVersionUID = 1L;
--- taco-reactive/src/main/java/com/abevilacqua/tacoreactive/repo/TacoRepository.java
+++ taco-reactive/src/main/java/com/abevilacqua/tacoreactive/repo/TacoRepository.java
@@ -3,10 +3,7 @@ package com.abevilacqua.tacoreactive.repo;
 import com.abevilacqua.tacoreactive.model.Taco;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
 
 @Repository
 public interface TacoRepository extends ReactiveCrudRepository<Taco, Long> {
-
-  Flux<Taco> findByOrOrderByCreatedAtDesc();
 }
