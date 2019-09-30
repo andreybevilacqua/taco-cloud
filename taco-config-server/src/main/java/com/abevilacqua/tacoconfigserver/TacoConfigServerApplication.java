d461a562397b8546e8728edeb5209110c9c4ef3e
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-30 21:33:27 +0200

    Adds Spring Config Server

---
+++ taco-config-server/src/main/java/com/abevilacqua/tacoconfigserver/TacoConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.abevilacqua.tacoconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoConfigServerApplication.class, args);
+	}
+
+}
---
+++ taco-config-server/src/test/java/com/abevilacqua/tacoconfigserver/TacoConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abevilacqua.tacoconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TacoConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
