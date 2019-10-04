65679844b4d23dfef72ad07a0b98ae885ab7fe20
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-10-04 11:39:34 +0200

    Adds handler interceptor with pre/post handler

--- taco/src/main/java/com/ab/taco/config/RequestLoggingFilter.java
+++ taco/src/main/java/com/ab/taco/config/RequestLoggingFilter.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 @Component
 public class RequestLoggingFilter extends OncePerRequestFilter {
 
-
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
--- taco/src/main/java/com/ab/taco/config/WebConfig.java
+++ taco/src/main/java/com/ab/taco/config/WebConfig.java
@@ -1,14 +1,22 @@
 package com.ab.taco.config;
 
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Setter(onMethod_ = @Autowired)
+    private RequestLoggingHandler requestLoggingHandler;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestLoggingHandler);
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
