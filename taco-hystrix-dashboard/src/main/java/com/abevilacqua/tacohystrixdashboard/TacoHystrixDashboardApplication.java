08d4a24289f5289241dc8eaf702679de9c3cce24
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-10-06 19:35:25 +0200

    Adds new project Taco-Hystrix-Dashboard to be responsible for the Hystrix dashboard

--- taco-hystrix-dashboard/src/main/java/com/abevilacqua/tacohystrixdashboard/TacoHystrixDashboardApplication.java
+++ taco-hystrix-dashboard/src/main/java/com/abevilacqua/tacohystrixdashboard/TacoHystrixDashboardApplication.java
@@ -2,8 +2,10 @@ package com.abevilacqua.tacohystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 public class TacoHystrixDashboardApplication {
 
 	public static void main(String[] args) {
