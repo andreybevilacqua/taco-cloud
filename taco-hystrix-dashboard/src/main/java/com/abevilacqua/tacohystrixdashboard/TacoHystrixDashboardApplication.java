3f0dcb20917d09dbb371903d29e1bea3e8b42ddd
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-10-06 19:31:26 +0200

    Removes Hystrix dashboard from both applications

---
+++ taco-hystrix-dashboard/src/main/java/com/abevilacqua/tacohystrixdashboard/TacoHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.abevilacqua.tacohystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoHystrixDashboardApplication.class, args);
+	}
+
+}
---
+++ taco-hystrix-dashboard/src/test/java/com/abevilacqua/tacohystrixdashboard/TacoHystrixDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abevilacqua.tacohystrixdashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TacoHystrixDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
