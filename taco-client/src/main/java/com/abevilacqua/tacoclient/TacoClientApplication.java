21ec046e16c88ff38c4071e02aaa9dcc1797962b
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-10-06 19:28:19 +0200

    Adds Hystrix dashboard in both Taco and Taco-Client projects

--- taco-client/src/main/java/com/abevilacqua/tacoclient/TacoClientApplication.java
+++ taco-client/src/main/java/com/abevilacqua/tacoclient/TacoClientApplication.java
@@ -3,9 +3,11 @@ package com.abevilacqua.tacoclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableHystrix
+@EnableHystrixDashboard
 public class TacoClientApplication {
 
 	public static void main(String[] args) {
--- taco/src/main/java/com/ab/taco/TacoApplication.java
+++ taco/src/main/java/com/ab/taco/TacoApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
 import static com.ab.taco.config.DBInitializer.initialize;
 
 @SpringBootApplication
 @EnableHystrix
+@EnableHystrixDashboard
 public class TacoApplication {
 
 	public static void main(String[] args) {
