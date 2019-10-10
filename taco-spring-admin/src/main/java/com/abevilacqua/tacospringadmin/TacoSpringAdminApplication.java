4441bdc83b16a080cd39937181e15cf9e0e93ae3
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-10-10 22:22:18 +0200

    Adds annotation @EnableAdminServer

--- taco-spring-admin/src/main/java/com/abevilacqua/tacospringadmin/TacoSpringAdminApplication.java
+++ taco-spring-admin/src/main/java/com/abevilacqua/tacospringadmin/TacoSpringAdminApplication.java
@@ -1,9 +1,11 @@
 package com.abevilacqua.tacospringadmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class TacoSpringAdminApplication {
 
 	public static void main(String[] args) {
