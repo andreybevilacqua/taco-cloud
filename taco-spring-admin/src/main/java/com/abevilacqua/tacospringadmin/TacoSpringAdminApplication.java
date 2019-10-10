276e99562173f55b750df26666c6991802c108df
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-10-10 22:21:12 +0200

    Adds Spring Admin application for read all Actuator endpoints from Taco apps

---
+++ taco-spring-admin/src/main/java/com/abevilacqua/tacospringadmin/TacoSpringAdminApplication.java
@@ -0,0 +1,13 @@
+package com.abevilacqua.tacospringadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoSpringAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoSpringAdminApplication.class, args);
+	}
+
+}
